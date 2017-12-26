88583f946d816933f4ba6ef9bc0ce4e3a07a9ea5
anixon604 <anixon604@gmail.com>
2017-12-26 01:29:39 +0100

    Refactored procedure and construction to pylint standards, added comments

--- GRASP/grasp.py
+++ GRASP/grasp.py
@@ -1,26 +1,59 @@
+# AMMM MIRI Project
+# Semester 1: 2017
+# Author: Anthony Nixon, Mathieu Chiavassa
+
 import math
+import sys
 import random
 
-def grasp(fx,gx,maxitr,xprime):
-    xprime = sys.maxsize
+
+def grasp_procedure(f_x, g_x, maxitr):
+    """ Procedure function
+        params:
+            fx - optimization function
+            gx - scoring function
+            maxitr - number of iterations to run construct/local cycle
+            returns: best solution after maxitr cycles"""
+    xprime = sys.maxint # best solution
+    alpha = 0.5 # greediness (0 - 1)
+
+    # iterate through multistart construct and local search cycles
+    # through each cycle, replace best solution if better found
     for i in range(maxitr):
-        # x param (psuedo) is only a reference. function can
-        # remove the argument and return a value instead
-        x = construct(gx,alpha)
-        x = local(fx,x) # see if var name ok
-        if fx(x) < fx(xprime):
-            xprime = x
+        temporary_solutionx = construct_grasp(g_x, alpha)
+        temporary_solutionx = localSearch(f_x, temporary_solutionx) # see if var name ok
+        if f_x(temporary_solutionx) < f_x(xprime):
+            xprime = temporary_solutionx
+        i = i+1
 
+    return xprime
 
-def constGrasp(gx,alpha):
-    x = []
-    C = []
-    while C != []:
-        solList = [gx(x) for x in C]
-        smin = min(solList)
-        smax = max(solList)
-        RCL = [s for s in C if g(s) <= smin+alpha*(smax-smin)]
-        s = random.choice(RCL)
-        x.append(s)
-        C.remove(s)
+# Constructor function - gene
+def construct_grasp(g_x, alpha):
+    """ Constructor function for GRASP
+        params:
+            gx - scoring function
+            alpha - greediness
+        returns: a valid solution (not necessarily optimal)"""
+    possible_solution_x = []
+    
+    # !!! init this to a matix/set of all possible schedules for
+    # all possible nurses
+    candidate_set = []
+    
+    while candidate_set != []:
+        # score all elements in the candidate set
+        scored_c_set = [g_x(elem) for elem in candidate_set]
+        smin = min(scored_c_set)
+        smax = max(scored_c_set)
+
+        # create RCL based on top scorers
+        restricted_c_list = [s for s in scored_c_set if s <= smin+alpha*(smax-smin)]
+        # choose random element from RCL to add to solution
+        solution_element = random.choice(restricted_c_list)
+        possible_solution_x.append(solution_element)
+        candidate_set.remove(solution_element)
+    return possible_solution_x
+
+def localSearch(fx, x):
     return x
