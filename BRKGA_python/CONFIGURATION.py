722186d4bd938cea2ba11098d27df1a8055a4403
MathieuChiavassa <34858496+MathieuChiavassa@users.noreply.github.com>
2017-12-30 23:58:58 +0100

    Update CONFIGURATION.py

--- BRKGA_python/CONFIGURATION.py
+++ BRKGA_python/CONFIGURATION.py
@@ -1,6 +1,3 @@
 from DATA_DUMMY import data
 
-
-config = {'chromosomeLength': data['nNurses']*data['nHours'], 'numIndividuals': 100, 'maxNumGen':5000, 'eliteProp':0.1, 'mutantProp':0.3,  'inheritanceProb':0.7}
-
-
+config = {'chromosomeLength': data['nNurses']*data['nHours'], 'numIndividuals': 150, 'maxNumGen':100, 'eliteProp':0.1, 'mutantProp':0.3,  'inheritanceProb':0.7}
