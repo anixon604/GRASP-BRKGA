564602e8702a3d883ecf77d884a62522ea455e24
MathieuChiavassa <34858496+MathieuChiavassa@users.noreply.github.com>
2017-12-30 23:59:46 +0100

    Update DATA_DUMMY.py

--- BRKGA_python/DATA_DUMMY.py
+++ BRKGA_python/DATA_DUMMY.py
@@ -1,8 +1,8 @@
-data={'nNurses': 20, 'nHours': 24, 'minHours':4, 'maxHours':8, 'maxPresence':10, 'maxConsec':4, 'demand':[10,12,11,11,10,15,9,8,8,10,11,14,9,10,16,11,8,14,7,13,9,15,12,10]}
+#TEST1
+#data={'nNurses': 3, 'nHours': 6, 'minHours':2, 'maxHours':5, 'maxPresence':5, 'maxConsec':3, 'demand':[0,2,1,1,0,1]}
 
+#TEST2
 #data={'nNurses': 10, 'nHours': 12, 'minHours':3, 'maxHours':7, 'maxPresence':10, 'maxConsec':4, 'demand':[0,2,4,5,2,7,6,4,3,3,1,1]}
 
-#data={'nNurses': 2, 'nHours': 6, 'minHours':2, 'maxHours':5, 'maxPresence':5, 'maxConsec':3, 'demand':[0,2,1,1,0,1]}
-
-
-#'demand':[10,12,11,11,10,15,9,8,8,10,11,14,9,10,16,11,8,14,7,13,9,15,12,10]
+#TEST3
+data={'nNurses': 25, 'nHours': 18, 'minHours':4, 'maxHours':8, 'maxPresence':10, 'maxConsec':5, 'demand':[9,8,8,10,11,12,9,10,16,11,10,14,13,13,9,15,12,10]}
