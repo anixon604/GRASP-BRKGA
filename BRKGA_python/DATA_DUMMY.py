1dccdbcca77946c78fc2023e49ab0c7360733314
MathieuChiavassa <34858496+MathieuChiavassa@users.noreply.github.com>
2018-01-09 18:34:34 +0100

    Add files via upload

--- BRKGA_python/DATA_DUMMY.py
+++ BRKGA_python/DATA_DUMMY.py
@@ -2,7 +2,20 @@
 #data={'nNurses': 3, 'nHours': 6, 'minHours':2, 'maxHours':5, 'maxPresence':5, 'maxConsec':3, 'demand':[0,2,1,1,0,1]}
 
 #TEST2
-#data={'nNurses': 10, 'nHours': 12, 'minHours':3, 'maxHours':7, 'maxPresence':10, 'maxConsec':4, 'demand':[0,2,4,5,2,7,6,4,3,3,1,1]}
+data={'nNurses': 10, 'nHours': 12, 'minHours':3, 'maxHours':7, 'maxPresence':10, 'maxConsec':4, 'demand':[0,2,4,5,2,7,6,4,3,3,1,1]}
 
 #TEST3
-data={'nNurses': 25, 'nHours': 18, 'minHours':4, 'maxHours':8, 'maxPresence':10, 'maxConsec':5, 'demand':[9,8,8,10,11,12,9,10,16,11,10,14,13,13,9,15,12,10]}
+#data={'nNurses': 25, 'nHours': 18, 'minHours':4, 'maxHours':8, 'maxPresence':10, 'maxConsec':5, 'demand':[9,8,8,10,11,12,9,10,16,11,10,14,13,13,9,15,12,10]}
+
+#SMALL
+#data = {'nNurses': 30, 'nHours':9, 'minHours': 3, 'maxHours': 6, 'maxConsec': 7, 'maxPresence': 8, 'demand': [5, 3, 8, 5, 1, 7, 5, 6, 2]}
+
+#MID
+#data = {'nNurses': 200, 'nHours':24, 'minHours': 6, 'maxHours': 12, 'maxConsec': 6, 'maxPresence': 18, 'demand': [53, 24, 33, 40, 70, 12, 33, 55, 66, 12, 30, 22, 55, 77, 88, 22, 34, 55, 22, 55, 23, 22, 11, 12]}
+
+#LARGE
+#data = {'nNurses': 1800, 'nHours':24, 'minHours': 6, 'maxHours': 18, 'maxConsec': 7, 'maxPresence': 24, 'demand': [964, 650, 966, 1021, 824, 387, 828, 952, 611, 468, 403, 561, 862, 597, 1098, 855, 918, 1016, 897, 356, 615, 670, 826, 349]}
+#'demand':[10,12,11,11,10,15,9,8,8,10,11,14,9,10,16,11,8,14,7,13,9,15,12,10]
+
+
+#data = {'nNurses': 25,'nHours':9,'minHours': 3,'maxHours': 6,'maxConsec': 7,'maxPresence': 8,'demand': [5, 3, 8, 5, 1, 7, 5, 6, 2]}
